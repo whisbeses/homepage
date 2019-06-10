f7c38f69cdc50896d4dac76afa3baa0304154bfa
Jeena Chemparathy <jeena@Jeenas-MacBook-Pro.local>
2019-06-09 18:29:34 -0700

    Add the nav bar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,29 +1,78 @@
 import React, { Component } from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import logo from '../../../logo.png'
+// import Navbar from 'react-bootstrap/Navbar';
 
 
-class Header extends Component {
+// class Header extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {}
+//   }
+
+//   render() {
+//     let icon = (
+//       <span class="logo">
+//         <a href="/">
+//           <img src={logo} height="33" width="120" alt="text here" /></a>
+//       </span>
+//     );
+//     return (
+//       <div>
+//         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+//           <Navbar.Brand href="#home">Whishbees</Navbar.Brand>
+//           <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+//           <Navbar.Collapse id="responsive-navbar-nav">
+//             <Nav className="mr-auto">
+//               <Nav.Link href="#post">Post</Nav.Link>
+//               <Nav.Link href="#game">Game</Nav.Link>
+//             </Nav>
+//           </Navbar.Collapse>
+//         </Navbar>
+//       </div>
+//     )
+//   }
+// }
+
+
+import {
+  Navbar,
+  NavbarBrand,
+  NavLink} from 'reactstrap';
+
+ class Header extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
   }
 
   render() {
+    let icon = (
+      <span class="logo">
+      <a href="/">
+        <img src={logo} width="10" alt="Whishbees" /></a>
+      </span>
+    );
     return (
-      <div>
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-          <Navbar.Brand href="#home">Whishbees</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#post">Post</Nav.Link>
-              <Nav.Link href="#game">Game</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
+      <div className="navBar">
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">{icon}</NavbarBrand>
+          <div className="pageOptions">
+            <NavLink href="/posts/">Post</NavLink>
+            <NavLink href="/game/">Game</NavLink>
+          </div>
         </Navbar>
       </div>
-    )
+    );
   }
 }
 
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,23 +2,49 @@ import React, { Component } from 'react';
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import Content from './Content.jsx';
+import axios from 'axios';
 
 
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state={}
+    this.state={
+      posts: []
+    }
+    this.getData = this.getData.bind(this);
+  }
+
+  componentWillMount() {
+    this.getData();
+  }
+
+  getData() {
+    axios.get('/api/posts')
+      .then((response) => {
+        console.log('Get request send to the server', response.data);
+        this.setState({
+          posts: response.data,
+        })
+      })
+      .catch((error) => {
+        console.log('Oops! something went wrong', error)
+      })
   }
 
   render() {
-    return (
-      <div className="home-container">
-        <Header />
-        {/* <Header />
-        <Content />
-        <Footer nameForClass={"home-footer"}/> */}
-      </div>
-    )
+    const { posts } = this.state;
+    if (posts !== []) {
+      console.log('posts: ', posts);
+      return (
+        <div className="home-container">
+          <Header />
+          {/* <Feed posts = {posts} /> */}
+          {/* <Header />
+          <Content />
+          <Footer nameForClass={"home-footer"}/> */}
+        </div>
+      )
+    }
   }
 }
 
